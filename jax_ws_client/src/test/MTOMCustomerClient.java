03b29da4b3f9a955d5b8a4689acd03edbfa1a3e2
al2wyw <yl0863758497@gmail.com>
2014-04-20 23:32:09 +0800

    MTOM client

---
+++ jax_ws_client/src/test/MTOMCustomerClient.java
@@ -0,0 +1,49 @@
+package test;
+
+import java.net.URL;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+
+import mtomupload.*;
+
+public class MTOMCustomerClient {
+
+         public static void main(String[] args) {
+
+                   // TODO Auto-generated method stub
+
+                   Customer customer = new Customer();
+
+                   String endPoint =
+
+ "http://10.30.152.147:8080/jax_ws/MTOMCustomerService?wsdl";
+
+                   try {
+
+                            MTOMCustomerService customerService = new MTOMCustomerService(
+
+                                               new URL(endPoint));
+
+                            MTOMCustomer customerPort = customerService
+
+                                               .getMTOMCustomerServicePort();
+
+                            customer.setId(101);
+
+                            customer.setName("mk");
+
+                            customer.setMyPhoto(new DataHandler(new FileDataSource(
+
+                                               "ans.jpg")));
+
+                            customerPort.uploadCustomerByName(customer);
+
+                   } catch (Exception e) {
+
+                            e.printStackTrace();
+
+                   }
+
+         }
+
+}
